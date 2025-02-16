e0fdc42bb1f2bc392e3255730306d042c59c1883
Aditya-Srivastava <pks85715@gmail.com>
2025-02-16 21:51:36 +0530

    footer design added

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 
 export const Footer = () => {
-    return (
-        <>
-        <div className="flex flex-col md:px-12 md:flex-row items-center justify-between">
-        <div>
-          <h1 className="text-white text-[4rem] font-extrabold font-jetbrainmono tracking-tighter">
-            EYE:)
-          </h1>
-          <h1 className="text-white text-[4rem] font-extrabold font-jetbrainmono tracking-tighter">
-            OPENING
-          </h1>
+  return (
+    <>
+      <div className="md:px-12">
+        <div className="flex flex-col  md:flex-row items-center justify-between">
+          <div>
+            <h1 className="text-white text-[4rem] font-extrabold font-jetbrainmono tracking-tighter">
+              EYE:)
+            </h1>
+            <h1 className="text-white text-[4rem] font-extrabold font-jetbrainmono tracking-tighter">
+              OPENING
+            </h1>
+          </div>
+          <div className="text-white">
+            <h1 className="text-white text-[4rem] font-extrabold font-jetbrainmono tracking-tighter">
+              DESIGNS AND DEVELOPMENT
+            </h1>
+          </div>
         </div>
-        <div>
-        <h1 className="text-white text-[4rem] font-extrabold font-jetbrainmono tracking-tighter">
-            DESIGNS:)
-          </h1>
+        <div className="text-white">
+          <div className="flex flex-col items-start justify-between">
+            <div className="mt-20 mb-10">
+              <p className="underline">Socials-</p>
+              <p className="underline">Linkedin</p>
+              <p className="underline">X</p>
+            </div>
+            <div className="mt-20 mb-10">
+              <p className="underline">Location-</p>
+              <p className="underline">India</p>
+            </div>
+            <div>
+              <p className="underline">Email-</p>
+              <p className="underline">aditysrivastava9850152@gmail.com</p>
+            </div>
+          </div>
         </div>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
