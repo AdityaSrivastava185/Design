16b633583a5e482435c757ea6ac4a034fa827f08
Aditya-Srivastava <pks85715@gmail.com>
2025-02-21 23:32:22 +0530

    image issue updated

--- src/app/components/note.jsx
+++ src/app/components/note.jsx
@@ -1,10 +1,10 @@
-'use client';
-import { useEffect, useRef, useState } from 'react';
-import styles from '../page.module.css'
-import Image from 'next/image';
+"use client";
+import { useEffect, useRef, useState } from "react";
+import styles from "../page.module.css";
+import Image from "next/image";
 // import Lenis from '@studio-freight/lenis'
-import Lenis from 'lenis';
-import { useTransform, useScroll, motion } from 'framer-motion';
+import Lenis from "lenis";
+import { useTransform, useScroll, motion } from "framer-motion";
 
 const images = [
   "1.jpg",
@@ -13,83 +13,80 @@ const images = [
   "2.png",
   "3.png",
   "4.jpg",
-  "design4.png", 
+  "design4.png",
   "1.jpg",
   "1.png",
   "2.jpg",
   "2.png",
   "3.png",
   "4.jpg",
-  "design4.png", 
-]
+  "design4.png",
+];
 
 export default function Home() {
-  
   const gallery = useRef(null);
-  const [dimension, setDimension] = useState({width:0, height:0});
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
 
   const { scrollYProgress } = useScroll({
     target: gallery,
-    offset: ['start end', 'end start']
-  })
+    offset: ["start end", "end start"],
+  });
   const { height } = dimension;
-  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2])
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3])
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25])
-  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
 
-  useEffect( () => {
-    const lenis = new Lenis()
+  useEffect(() => {
+    const lenis = new Lenis();
 
     const raf = (time) => {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
 
     const resize = () => {
-      setDimension({width: window.innerWidth, height: window.innerHeight})
-    }
+      setDimension({ width: window.innerWidth, height: window.innerHeight });
+    };
 
-    window.addEventListener("resize", resize)
+    window.addEventListener("resize", resize);
     requestAnimationFrame(raf);
     resize();
 
     return () => {
       window.removeEventListener("resize", resize);
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.spacer}></div>
       <div ref={gallery} className={styles.gallery}>
-        <Column images={[images[0], images[1], images[2]]} y={y}/>
-        <Column images={[images[3], images[4], images[5]]} y={y2}/>
-        <Column images={[images[6], images[7], images[8]]} y={y3}/>
-        <Column images={[images[9], images[10], images[11]]} y={y4}/>
+        <Column images={[images[0], images[1], images[2]]} y={y} />
+        <Column images={[images[3], images[4], images[5]]} y={y2} />
+        <Column images={[images[6], images[7], images[8]]} y={y3} />
+        <Column images={[images[9], images[10], images[11]]} y={y4} />
       </div>
       <div className={styles.spacer}></div>
     </main>
-  )
+  );
 }
 
-const Column = ({images, y}) => {
+const Column = ({ images, y }) => {
   return (
-    <motion.div 
-      className={styles.column}
-      style={{y}}
-      >
-      {
-        images.map( (src, i) => {
-          return <div key={i} className={styles.imageContainer}>
-            <Image 
+    <motion.div className={styles.column} style={{ y }}>
+      {images.map((src, i) => {
+        return (
+          <div key={i} className={styles.imageContainer}>
+            <Image
               src={`/${src}`}
-              alt='image'
+              alt="Gallery Image"
               fill
+              className={styles.img}
             />
           </div>
-        })
-      }
+        );
+      })}
     </motion.div>
-  )
-}
+  );
+};
