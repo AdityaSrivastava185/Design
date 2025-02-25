0b9f1daa8f4c5e556f534e08eca55425a193577d
Aditya-Srivastava <pks85715@gmail.com>
2025-02-25 22:31:17 +0530

    description styles changes

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -8,7 +8,7 @@ const About = () => {
           className=" mb-9 md:mb-0
         w-full sm:max-w-[60vw] md:max-w-[40vw] mt-[6vh] md:mt-0"
         >
-          <h1 className=" font-bold text-white uppercase text-[8vw] sm:text-[6vw] md:text-[6vw] lg:text-[5vw] sm:leading-[1.1] md:leading-7 lg:leading-[70px] tracking-tighter leading-8 font-jetbrainmono">
+          <h1 className=" text-white uppercase text-[8vw] sm:text-[6vw] md:text-[6vw] lg:text-[5vw] sm:leading-[1.1] md:leading-7 lg:leading-[70px] tracking-tighter leading-8 font-jetbrainmono font-extrabold">
             you think it's boring,think again...:)
           </h1>
         </div>
@@ -72,7 +72,7 @@ const About = () => {
                 {phase.number}
               </div>
               <div className="max-w-[80vw] sm:max-w-[70vw] md:max-w-[50vw] mb-[50px] md:mb-[0px] lg:pb-10">
-                <h2 className="text-white max-w-[40vw] md:max-w-[0vw] text-[9vw] sm:text-[4vw] md:text-[2vw] font-bold mb-[10px] md:mb-[0px] uppercase tracking-tighter leading-8 md:leading-none font-jetbrainmono lg:pb-8">
+                <h2 className="text-white max-w-[40vw] md:max-w-[0vw] text-[9vw] sm:text-[4vw] md:text-[2vw] mb-[10px] md:mb-[0px] uppercase tracking-tighter leading-8 md:leading-none font-jetbrainmono lg:pb-8">
                   {phase.title}
                 </h2>
                 <p className="text-zinc-500 text-[4.5vw] sm:text-[3vw] md:text-[1vw] font-semibold leading-5 tracking-tighter lg:text-xl">
